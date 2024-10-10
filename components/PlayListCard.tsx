10bbf3695bb2e780f2f2aeb5acb7d308d52c08b4
권경민 <phillip0919@gmail.com>
2024-10-10 10:14:35 +0900

    <feat>: 7-5 playlist 이슈 수정

--- components/PlayListCard.tsx
+++ components/PlayListCard.tsx
@@ -1,15 +1,35 @@
+"use client";
+
 import { Playlist } from "@/types";
+import Image from "next/image";
 import React from "react";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const PlayListCard = ({ playlist }: { playlist: Playlist }) => {
-  const {} = 
-  return (
-    <article>
-      <section></section>
-      <section>
-        <div>
+  const { push } = useRouter();
+  const { owner, playlistName, songList } = playlist;
 
-        </div>
+  const songListLen = songList.length;
+  const imgSrc = getRandomElementFromArray(songList).imageSrc;
+
+  const onClickCard = (id) => {
+    push(`/playlist?list=${id}`);
+  };
+
+  return (
+    <article className="h-[240px] cursor-pointer">
+      <section onClick={onClickCard} className="relative h-[136px]">
+        <Image
+          src={imgSrc}
+          fill={true}
+          alt="thumbNail"
+          className="object-cover"
+        />
+      </section>
+      <section className="mt-2">
+        <div>{playlistName}</div>
+        <div className="text-neutral-500">{`${owner} - 트랙 ${songListLen}개`}</div>
       </section>
     </article>
   );
